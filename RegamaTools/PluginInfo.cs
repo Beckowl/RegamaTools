1b0c0bcdb3f01efb49658530a34e33ddb69bb8b5
pipocalio <bowserkoopah@gmail.com>
2025-06-26 20:50:31 -0300

    Bump version number

--- RegamaTools/PluginInfo.cs
+++ RegamaTools/PluginInfo.cs
@@ -8,5 +8,5 @@ internal class PluginInfo
 {
     public const string NAME = "RegamaTools";
     public const string GUID = "Beckowl.RegamaTools";
-    public const string VERSION = "0.1.0";
+    public const string VERSION = "0.1.1";
 }
